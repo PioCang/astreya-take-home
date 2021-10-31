a167870196f7bcaf2a7c269d5ad47993b1936072
Vince <piovincecang@gmail.com>
2021-10-31 13:10:22 -0400

    Registered models to django admin

--- django_backend/janken/admin.py
+++ django_backend/janken/admin.py
@@ -1,3 +1,9 @@
+""" For admin UI debugging """
+
 from django.contrib import admin
+from .models import Match, Player, Round
 
 # Register your models here.
+admin.site.register(Match)
+admin.site.register(Player)
+admin.site.register(Round)
